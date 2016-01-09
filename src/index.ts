4cbb801d78d07a67857ce642eeb762f221a7c0a5
Eryk Warren <emw@mwarren.com>
2016-01-09 17:50:42 -0500

    fix backticks

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export class Foo2 {
     const foo1 = new Foo1();
     const exe = process.execPath;
     const name = path.basename(exe);
-    return 'I am Foo2. This process running this is ${name}. Calling Foo1:' +
+    return `I am Foo2. This process running this is ${name}. Calling Foo1:` +
       foo1.talk();
   }
 }
