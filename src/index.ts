58fec69dba852a19647af149aa80a541d721a318
Eryk Warren <emw@mwarren.com>
2016-01-09 14:52:14 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+import * as path from 'path';
+import { Foo1 } from 'ts-lib1';
+
+export class Foo2 {
+  talk(): string {
+    const foo1 = new Foo1();
+    const exe = process.execPath;
+    const name = path.basename(exe);
+    return 'I am Foo2. This process running this is ${name}. Calling Foo1:' +
+      foo1.talk();
+  }
+}
